3edf36db60d1b48e893c0e79824b192428ae0228
9M2PJU <9m2pju@gmail.com>
2026-01-15 11:31:51 +0800

    Remove GitHub logo from footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -13,15 +13,6 @@ const showDonateModal = ref(false)
       <div i-carbon-home class="text-sm"></div>
     </a>
     
-    <a
-      href="https://github.com/9M2PJU/9M2PJU-PDF-Watermark"
-      target="_blank"
-      class="p-2 rounded-lg bg-slate-700/50 hover:bg-slate-600 text-gray-400 hover:text-white transition-colors"
-      title="View on GitHub"
-    >
-      <div i-carbon-logo-github class="text-sm"></div>
-    </a>
-
     <!-- Donate Button -->
     <button
       @click="showDonateModal = true"
